efe13eef5b6211b007e359df267f3a50de24c22a
Grant Herrema <granther321@gmail.com>
2024-08-26 20:27:15 -0400

    Added history chatuuid ability

--- inference_client.py
+++ inference_client.py
@@ -30,6 +30,7 @@ class InferenceClient():
         self.global_sys_prompt = global_sys_prompt
         self.backend = backend.lower()
         self.temperature = temperature
+        self.history = dict()
 
         if backend not in self.backends:
             raise ValueError(f"backend must be one of these: {self.backends}")
@@ -44,24 +45,43 @@ class InferenceClient():
         elif backend == "openai":
             self.client = OpenAI(api_key=api_key or os.getenv(openai_key_name), base_url=inference_url)
 
+    
+    def _get_session_history(self, chatuuid: str) -> list:
+        if chatuuid not in self.history:
+            self.history[chatuuid] = list()
 
-    def client_infer(self, prompt, sys_prompt:str=None, model:str=None, stream:bool=False, max_new_tokens:int=None, temperature:float=None):
+        if chatuuid is None:
+            return list()
+        
+        return self.history[chatuuid]
+    
+    def clear_session_history(self, chatuuid: str):
+        self.history[chatuuid] = []
+
+    def user_infer_stream(self, chatuuid: str = "abc123", user_prompt: str = None, sys_prompt: str = None):
+        history = self._get_session_history(chatuuid)
+
+        if sys_prompt != None:
+            history.append({"role":"user", "content":user_prompt})
+            history.append({"role":"system", "content":sys_prompt})
+        else:
+            history.append({"role":"user", "content":user_prompt})
+
+
+    def simple_infer(self, prompt, sys_prompt:str=None, model:str=None, stream:bool=False, max_new_tokens:int=None, temperature:float=None, chatuuid:str=None):
         model = model or self.model
         sys_prompt = sys_prompt or self.global_sys_prompt
         max_new_tokens = max_new_tokens or self.max_new_tokens
         temperature = temperature or self.temperature
 
+        history = self._get_session_history(chatuuid)
+
+        if sys_prompt:
+            history.append({"role": "system", "content": sys_prompt})
+        history.append({"role":"user", "content": prompt})
+
         response = self.client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": "you are a helpful assistant."
-                },
-                {
-                    "role": "user",
-                    "content": "Explain the importance of fast language models",
-                }
-            ],
+            messages=history,
             model=model,
             temperature=temperature,
             max_tokens=max_new_tokens,
@@ -69,12 +89,15 @@ class InferenceClient():
             stream=stream,
         )
 
-        if stream is True:
+        if stream:
             # Returns generator func
-            return self.process_stream(response)
+            self.process_stream(response, history)
 
-        elif stream is False:
-            return response.choices[0].message.content
+        else:
+            # Non-streaming case
+            message = response.choices[0].message.content
+            history.append({"role":"assistant", "content": message})
+            return message
         
     def set_sys_prompt(self, new_sys):
         self.global_sys_prompt = new_sys
@@ -83,13 +106,21 @@ class InferenceClient():
         self.model = new_model
 
     # Function that is a generator
-    def process_stream(self, stream):
+    def process_stream(self, response, history):
         partial_message = ""
-        for chunk in stream:
+        for chunk in response:
             if chunk.choices[0].delta.content is not None:
                 partial_message = partial_message + chunk.choices[0].delta.content
                 yield partial_message
 
+        history.append({"role":"assistant", "content": partial_message})
+
+        
+
 if __name__ == "__main__":
-    infer = InferenceClient(model="gemma2-9b-it", api_key="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", verbose=True)
-    print(infer.client_infer("Heloe"))
+    # infer = InferenceClient(model="gemma2-9b-it", api_key="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", verbose=True)
+    # x = infer.simple_infer("My name is bob", stream=False, chatuuid="123")
+    # print(x)
+    # x = infer.simple_infer("What is my name", stream=False, chatuuid="123")
+    # print(x)
+    pass
